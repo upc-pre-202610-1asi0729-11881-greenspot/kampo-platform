a24098ad803abc7848105c99a9dd21fda78ce9ac
rommelDN <rommelhb2002daniel@gmail.com>
2026-05-31 22:41:47 -0500

    firts commit

---
+++ src/main/java/com/acme/kampo/platform/KampoPlatformApplication.java
@@ -0,0 +1,13 @@
+package com.acme.kampo.platform;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KampoPlatformApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(KampoPlatformApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/acme/kampo/platform/KampoPlatformApplicationTests.java
@@ -0,0 +1,13 @@
+package com.acme.kampo.platform;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KampoPlatformApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
